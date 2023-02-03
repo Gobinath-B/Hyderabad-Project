156263611afc434b071897fa3c6141506a812966
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-02-03 10:40:28 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var login = require('./routes/login')
 var app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use("/", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
@@ -11,10 +14,10 @@ app.get("/product", (req, res) => res.render("product"));
 app.get("/product/:id", (req, res) => res.render("single-product"));
 app.get("/clients", (req, res) => res.render("client"));
 app.get("/history", (req, res) => res.render("history"));
-app.get("/dashboard", (req,res) => res.render("admin/dashboard"))
-app.get("/edit", (req,res) => res.render("admin/edit"))
-app.get("/add", (req,res) => res.render("admin/add"))
-app.get("/login", (req,res) => res.render("admin/login"))
+app.get("/admin/dashboard", (req,res) => res.render("admin/dashboard"))
+app.get("/admin/edit", (req,res) => res.render("admin/edit"))
+app.get("/admin/add", (req,res) => res.render("admin/add"))
+app.use("/admin/login",login)
 
 
 var server = app.listen(3000, function () {
