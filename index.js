b3ea6b7eb4f5afe497c0c6f530442f9723f6e8b1
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-02-05 19:21:55 +0530

    (features) : added two new feature
    list all products
    show product single

--- index.js
+++ index.js
@@ -8,13 +8,38 @@ const fb = db.firestore()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/", express.static(__dirname + "/public"));
+app.use("/product", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/about", (req, res) => res.render("about"));
 app.get("/contact", (req, res) => res.render("contact"));
-app.get("/product", (req, res) => res.render("product"));
-app.get("/product/:id", (req, res) => res.render("single-product"));
+app.get("/product", (req, res) => {
+
+  fb.collection('products').get().then(e=>{
+    const data = []
+    const doc = e.docs.forEach(e=>{
+        data.push({...e.data(),id:e.id})
+    })
+    console.log();
+    res.render("product",{data:data})
+  })
+});
+app.get("/product/:id", async(req, res) =>{
+  const id = req.params.id
+  const relate=[]
+  const data =  await fb.collection('products').doc(id).get()
+  const cat =  await fb.collection('products').get()
+  cat.forEach(doc=>{
+    if(data.data().pcat==doc.data().pcat){
+      if(data.id!=doc.id){
+        relate.push({...doc.data(),id:doc.id})
+      }
+    }
+  })
+  
+  res.render('singleproduct',{data:{...data.data(),id:data.id},relate:relate})
+});
 app.get("/clients", (req, res) => res.render("client"));
 app.get("/history", (req, res) => res.render("history"));
 
